451edbf3c34968e169869e83f4912e649c05c32e
pukssa <argen@MacBook-Air--puksa.local>
2025-01-14 15:17:50 +0600

    Init: Lesson 1, month 2, theme: OOP

---
+++ hw1.py
@@ -0,0 +1,31 @@
+class Hero:
+    def __init__(self, name, lvl, HP):
+        self.name = name
+        self.lvl = lvl
+        self.HP = HP
+
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, мой lvl {self.lvl}, мое HP {self.HP}")
+
+    def is_adult(self):
+        return self.lvl >= 10
+
+    def __str__(self):
+        return f"Имя: {self.name}, lvl: {self.lvl}, HP: {self.HP}"
+
+
+hero1 = Hero("Тимоша", 5, 100)
+hero2 = Hero("Иванушка", 12, 150)
+hero3 = Hero("Ерема", 8, 120)
+
+hero1.introduce()
+hero2.introduce()
+hero3.introduce()
+
+print(hero1.is_adult())
+print(hero2.is_adult())
+print(hero3.is_adult())
+
+print(hero1)
+print(hero2)
+print(hero3)
